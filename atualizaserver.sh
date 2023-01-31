8e2bf5de538738436af7d8d2bd09f467376c2e4d
miguel kallemnack <miguelkallemback.seven@gmail.com>
2023-01-31 00:04:02 -0300

    adicionado copia server

---
+++ atualizaserver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker stop $(docker ps -aq)
+
+docker rm $(docker ps -aq)
+
+docker rmi $(docker images -q)
+
+cd /
+
+if [ -d "nboxatt" ]; then
+rm -r -d nboxatt
+fi
+
+git clone https://github.com/aloproducao/nboxatt
+
+cd nboxatt
+
+docker build -t nboxatt .
+
+docker run -d --net=host -e INCLUDE_PUBLIC_IP_IN_NAT_1_TO_1_IP=yes nboxatt
