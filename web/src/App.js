5b167842bab0b490f7195ac6436152c058bc585e
miguel kallemnack <miguelkallemback.seven@gmail.com>
2023-02-05 19:48:23 -0300

    FIx google ads

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
 import Selection from './components/selection'
 import Player from './components/player'
+import Analytics from 'analytics'
+import googleTagManager from '@analytics/google-tag-manager'
+
+const analytics = Analytics({
+  app: 'awesome-app',
+  plugins: [
+    googleTagManager({
+      containerId: 'G-7M5G85V0YE'
+    })
+  ]
+})
 
 function App() {
   return (
